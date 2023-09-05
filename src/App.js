a3ea522dd6a2ebeff6cf26dc04d1bfea4fd5c42f
Spencer Norris <Spencer.serv@gmail.com>
2023-09-05 16:01:21 -0500

    All Posts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,44 @@
+import './Apps.css'
+import { useEffect, useState } from "react"
+import { getAllPosts, getExpandedPosts } from "./services/getPosts"
+
 
 export const App = () => {
-  return <div>Hello World!</div>
+  const [allPosts, setAllPosts] = useState([])
+  const [expandedPosts, setExpandedPosts] = useState([])
+
+
+    useEffect(() => {
+      getAllPosts().then((postArray) => {
+        setAllPosts(postArray)
+        console.log(postArray)
+      })
+
+      getExpandedPosts().then((expandedArray) => {
+        setExpandedPosts(expandedArray)
+        console.log(expandedArray)
+      })
+
+
+    }, [])
+
+
+
+  return ( <div>
+
+  <div>
+    <h2 className='post-heading'>Current Posts</h2>
+    <div className='posts'>
+    {expandedPosts.map((post) => {
+      return (
+        <section className='post-box' key={post.id}>
+          <h3 className='post-title'>{post.title}</h3><br></br>
+          <p className='post-topic'>{post.topic.name}</p>
+        </section>
+      )
+    })}</div>
+  </div>
+
+  </div>
+  )
 }
---
+++ src/services/getPosts.js
@@ -0,0 +1,7 @@
+export const getAllPosts = () => {
+    return fetch('http://localhost:8088/posts').then(res => res.json())
+}
+
+export const getExpandedPosts = () => {
+    return fetch('http://localhost:8088/posts?_expand=topic').then(res => res.json())
+}
