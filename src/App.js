66b71380b3d4f6e520e954feeac48569f433f7e7
Spencer Norris <Spencer.serv@gmail.com>
2023-09-06 14:16:33 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,44 +1,92 @@
-import './Apps.css'
-import { useEffect, useState } from "react"
-import { getAllPosts, getExpandedPosts } from "./services/getPosts"
-
+import "./Apps.css";
+import { useEffect, useState } from "react";
+import { getAllPosts } from "./services/getPosts";
+import { getAllTopics } from "./services/getTopics";
 
 export const App = () => {
-  const [allPosts, setAllPosts] = useState([])
-  const [expandedPosts, setExpandedPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([]);
+  const [expandedPosts, setExpandedPosts] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [chosenTopic, setChosenTopic] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  useEffect(() => {
+    getAllPosts().then((postArray) => {
+      setAllPosts(postArray);
+      console.log(postArray);
+    });
 
-    useEffect(() => {
-      getAllPosts().then((postArray) => {
-        setAllPosts(postArray)
-        console.log(postArray)
-      })
+    getAllTopics().then((topicsArray) => {
+      setTopics(topicsArray);
+      console.log(topicsArray);
+    });
+  }, []);
 
-      getExpandedPosts().then((expandedArray) => {
-        setExpandedPosts(expandedArray)
-        console.log(expandedArray)
-      })
+  useEffect(() => {
+    if (chosenTopic > 0) {
+      const postTopic = allPosts.filter(
+        (post) => post.topic.id === (parseInt(chosenTopic))
+      );
+      setExpandedPosts(postTopic);
+    } else {
+      setExpandedPosts(allPosts);
+    }
+  }, [chosenTopic, allPosts]);
 
+  useEffect(() => {
+    const foundTerm = allPosts.filter((post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setExpandedPosts(foundTerm);
+  }, [searchTerm, allPosts]);
 
-    }, [])
+  return (
+    <div>
+      <div>
+        <div className="heading-top">
+          <div className="filter-bar">
+            <select onChange={(event) => {
+                      setChosenTopic(event.target.value);
+                    }} type="filter">
+              <option value="0">Select Topic...</option>
+              {topics.map((topic) => {
+                return (
+                  <option
+                    
+                    value={topic.id}
+                    key={topic.id}
+                  >
+                    {topic.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <h2 className="post-heading">Current Posts</h2>
 
-
-
-  return ( <div>
-
-  <div>
-    <h2 className='post-heading'>Current Posts</h2>
-    <div className='posts'>
-    {expandedPosts.map((post) => {
-      return (
-        <section className='post-box' key={post.id}>
-          <h3 className='post-title'>{post.title}</h3><br></br>
-          <p className='post-topic'>{post.topic.name}</p>
-        </section>
-      )
-    })}</div>
-  </div>
-
-  </div>
-  )
-}
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="search-field"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="posts">
+          {expandedPosts.map((post) => {
+            return (
+              <section className="post-box" key={post.id}>
+                <h3 className="post-title">{post.title}</h3>
+                <br></br>
+                <p className="post-topic">{post.topic.name}</p>
+              </section>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/services/getPosts.js
+++ src/services/getPosts.js
@@ -1,5 +1,5 @@
 export const getAllPosts = () => {
-    return fetch('http://localhost:8088/posts').then(res => res.json())
+    return fetch('http://localhost:8088/posts?_expand=topic').then(res => res.json())
 }
 
 export const getExpandedPosts = () => {
